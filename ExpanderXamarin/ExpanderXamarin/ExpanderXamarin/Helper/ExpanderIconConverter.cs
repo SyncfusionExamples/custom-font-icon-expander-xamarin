35296d06e2ff796413d51d4d22e803d1d1d8c11a
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-28 12:05:54 +0530

    XAMARIN-34597-Sample and screen shot added.

---
+++ ExpanderXamarin/ExpanderXamarin/ExpanderXamarin/Helper/ExpanderIconConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ExpanderXamarin
+{
+ public class ExpanderIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                if ((string)parameter == "Android")
+                    return "\ue704";
+                else if((string)parameter == "iOS")
+                    return "\ue700";
+                else
+                    return "\ue702";
+            }
+            else
+            {
+                if ((string)parameter == "Android")
+                    return "\ue705";
+                else if ((string)parameter == "iOS")
+                    return "\ue701";
+                else
+                    return "\ue703";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
